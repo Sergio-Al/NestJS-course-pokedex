a14bba70346cb4e9f1c92ff3a5e84ac874804586
Sergio-Al <sergio.alejandro.ml900@gmail.com>
2022-12-03 22:04:52 -0400

    Fetching data from an API
    Now we are fetching data fro m pokemon api and we are using an alpha version of axios

---
+++ src/seed/seed.service.ts
@@ -0,0 +1,21 @@
+import { PokeResponse } from './interfaces/poke-response.interface';
+import { Injectable } from '@nestjs/common';
+import axios, { AxiosInstance } from 'axios';
+
+@Injectable()
+export class SeedService {
+  private readonly axios: AxiosInstance = axios;
+
+  async executeSeed() {
+    const { data } = await this.axios.get<PokeResponse>(
+      'https://pokeapi.co/api/v2/pokemon?limit=10',
+    );
+
+    data.results.forEach(({ name, url }) => {
+      const segments = url.split('/');
+      const no = +segments[segments.length - 2];
+      console.log({ name, no });
+    });
+    return data.results;
+  }
+}
