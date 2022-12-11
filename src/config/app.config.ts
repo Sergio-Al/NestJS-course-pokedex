62c52649309285b3ef2a02ff3bf586361f8f9330
Sergio-Al <sergio.alejandro.ml900@gmail.com>
2022-12-11 00:05:09 -0400

    Joi
    We just installed Joi to validate our config schema.
    - Notice that joi validates the env schema first, so if we create defaults in joi the default established in our app.config won't work because it's already created a default by joi

--- src/config/app.config.ts
+++ src/config/app.config.ts
@@ -2,5 +2,6 @@ export const EnvConfiguration = () => ({
   environment: process.env.NODE_ENV || 'dev',
   mongodb: process.env.MONGODB,
   port: process.env.PORT || 3002,
-  defaultLimit: process.env.DEFAULT_LIMIT || 7,
+  // we are casting the validation (joi) default to number with '+'
+  defaultLimit: +process.env.DEFAULT_LIMIT || 7,
 });
